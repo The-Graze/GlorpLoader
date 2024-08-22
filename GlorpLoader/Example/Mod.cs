ab3580a5ba8c3cbdc0762283766e828613b2a1a5
biotest05 <169047576+biotest05real@users.noreply.github.com>
2024-08-22 20:58:05 +0200

    Added some stuff

---
+++ GlorpLoader/Example/Mod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GlorpLoader;
+
+namespace GlorpLoader.Example
+{
+    [GlorpMod("Glorp.ExampleMod", "ExampleMod", "0.0.1")]
+    public class Mod : GlorpClass
+    {
+    }
+}
